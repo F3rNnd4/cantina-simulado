bd993c9661185c06e2f84b294ff1c3ce513e981c
F3rNnd4 <fernandaalves.tds2.senai@gmail.com>
2025-11-13 09:26:35 -0300

    feat: create project with seed.sql

---
+++ seed.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE cantina;
+\c cantina;
+
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100),
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(100) NOT NULL,
+    funcao VARCHAR(20) DEFAULT 'funcionario'
+);
+
+CREATE TABLE produtos (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE estoque (
+    id SERIAL PRIMARY KEY,
+    produto_id INT REFERENCES produtos(id),
+    quantidade INT NOT NULL
+);
+
+CREATE TABLE vendas (
+    id SERIAL PRIMARY KEY,
+    usuario_id INT REFERENCES usuarios(id),
+    produto_id INT REFERENCES produtos(id),
+    quantidade_produto INT NOT NULL
+);
+
+INSERT INTO usuarios (nome, email, senha, funcao) VALUES
+('Maryna Silva', 'mary.silva@gmail.com', 'marynaA123', 'admin'),
+('Roberto Lima', 'roberto.lima@gmail.com', 'roberto123', 'funcionario');
+
+INSERT INTO produtos (nome, preco) VALUES
+('Salgado assado de frango', 7.0),
+('Refrigerante lata 350ml', 5.0);
+
+INSERT INTO estoque (produto_id, quantidade) VALUES
+(1, 100),
+(2, 200);
+
+INSERT INTO vendas (usuario_id, produto_id, quantidade_produto) VALUES 
+(2, 2, 3),
+(1, 2, 1),
+(1, 1, 1);
